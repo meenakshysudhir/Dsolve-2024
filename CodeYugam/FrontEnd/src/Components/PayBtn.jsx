cef9956fe180dfbc6988456b6d6951164143da47
Ski <skicodeoo7@gmail.com>
2024-04-05 17:59:20 +0530

    ere

--- CodeYugam/FrontEnd/src/Components/PayBtn.jsx
+++ CodeYugam/FrontEnd/src/Components/PayBtn.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+// import { useState } from "react";
 import "../Pages/Profile.css";
+import axios from "axios";
 const PayBtn = ({ to }) => {
   const navigate = useNavigate();
+  //   const [dataToSend, setDataToSend] = useState;
   const handleClick = () => {
     navigate(to);
   };

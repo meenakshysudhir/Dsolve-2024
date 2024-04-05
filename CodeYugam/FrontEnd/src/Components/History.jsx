c959d1ab2c743b48825e452eb85c94131ac74caf
Ski <skicodeoo7@gmail.com>
2024-04-05 14:00:34 +0530

    ewew

--- CodeYugam/FrontEnd/src/Components/History.jsx
+++ CodeYugam/FrontEnd/src/Components/History.jsx
@@ -3,15 +3,29 @@ import { useState, useEffect } from "react";
 import "../Pages/Profile.css";
 const History = () => {
   const [historyData, setHistoryData] = useState([
-    ["April", 220321, "22LH100", "Anu", 2003, 1200, 200, "unpaid"],
-    [" March", 220311, "21LH140", "Anu", 3000, 500, 800, "paid"],
-    ["Feb", 220521, "22LH105", "Anu", 2003, 1200, 200, "paid"],
-    ["Jan", 220891, "22LH103", "Anu", 2042, 2200, 100, "paid"],
+    // ["April", 220321, "22LH100", "Anu", 2003, 1200, 200, "unpaid"],
+    // [" March", 220311, "21LH140", "Anu", 3000, 500, 800, "paid"],
+    // ["Feb", 220521, "22LH105", "Anu", 2003, 1200, 200, "paid"],
+    // ["Jan", 220891, "22LH103", "Anu", 2042, 2200, 100, "paid"],
     // [1, 1, 1, 1, 1, 1, 1, "unpaid"],
     // [1, 1, 1, 1, 1, 1, 1, "unpaid"],
     // [1, 1, 1, 1, 1, 1, 1, "unpaid"],
     // [1, 1, 1, 1, 1, 1, 1, "unpaid"],
   ]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/bill");
+        const jsonData = await response.json();
+        setHistoryData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="AllBills">
